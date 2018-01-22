dfd8dab79c8c8fa5a42f4f4d362fdb66197ff3e3
mridul-arora <aroramridul18@gmail.com>
2018-01-22 22:25:57 +0530

    update

--- PR_Checkboxes/Date/datetime/process.php
+++ PR_Checkboxes/Date/datetime/process.php
@@ -12,7 +12,7 @@ foreach($checkbox1 as $chk1)
    {
       $chk .= $chk1.",";
    }
-$in_ch=mysqli_query($connect,"INSERT INTO slots('name,date,one,two,three,four,five,six,seven,eight') VALUES ('$chk')");
+$in_ch=mysqli_query($connect,"INSERT INTO slots(name,date,one,two,three,four,five,six,seven,eight) VALUES ($chk)");
 if(mysqli_affected_rows($connect) > 0){
 	echo "<p>Employee Added</p>";
 	echo "<a href=index.php>Go Back</a>";
