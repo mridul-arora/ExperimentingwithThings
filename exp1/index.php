07e451776df7b49f5b70ccb7bc613cc005eec703
mridul-arora <aroramridul18@gmail.com>
2018-05-28 20:01:21 +0530

    renamed PR Checkboxes

---
+++ exp1/index.php
@@ -0,0 +1,75 @@
+<html>
+<style>
+div {
+  display: table;
+  display: table-cell;
+  display: table-column;
+  display: table-column-group;
+  display: table-header-group;
+  display: table-row-group;
+  display: table-footer-group;
+  display: table-row;
+  display: table-caption;
+
+}
+    .cell{
+        margin-left: 10px;
+        width:16px;
+        background-color: cornflowerblue;
+        border:4px bold white;
+    }
+</style>
+<body>
+    <div style="display: table;">
+  <div style="display: table-row;">
+    <div style="display: table-cell;" class="cell">
+      Gross
+    </div>
+       <div style="display: table-cell;" class="cell">
+      Gross
+    </div>
+       <div style="display: table-cell;" class="cell">
+      Gross
+    </div>
+       <div style="display: table-cell;" class="cell">
+      Gross
+    </div>
+       <div style="display: table-cell;" class="cell">
+      Gross
+    </div>
+       <div style="display: table-cell;" class="cell">
+      Gross
+    </div>
+       <div style="display: table-cell;" class="cell">
+      Gross
+    </div>
+
+  </div>
+
+  <div style="display: table-row;">
+    <div style="display: table-cell;" class="cell">
+      Gross
+    </div>
+       <div style="display: table-cell;" class="cell">
+      Gross
+    </div>
+       <div style="display: table-cell;" class="cell">
+      Gross
+    </div>
+       <div style="display: table-cell;" class="cell">
+      Gross
+    </div>
+       <div style="display: table-cell;" class="cell">
+      Gross
+    </div>
+       <div style="display: table-cell;" class="cell">
+      Gross
+    </div>
+       <div style="display: table-cell;" class="cell">
+      Gross
+    </div>
+
+  </div>
+</div>
+    </body>
+</html>
